eb522cc511a55fe83b0d32e4ccf56d6d24a24cf8
cskstrngth <cskstrngth@gmail.com>
2016-03-29 18:53:20 -0400

    Add cookbook files generated by chef.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'roshicookbook'
+maintainer 'David Jee (cskstrngth)'
+maintainer_email 'cskstrngth@gmail.com'
+license 'all_rights'
+description 'Installs/Configures a roshi stack'
+long_description 'Installs/Configures a simple CRDT stack based on roshi'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: roshicookbook
+# Recipe:: default
+#
+# Copyright (c) 2016 David Jee (cskstrngth), All Rights Reserved.
