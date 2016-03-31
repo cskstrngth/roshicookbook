26c4fe273b9f54088f0a6d9ce78ee2c0195ba8ce
cskstrngth <cskstrngth@gmail.com>
2016-03-30 22:36:33 -0400

    Add nginx.conf.erb template resource to enable reverse proxy functionality.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,14 @@
 default['firewall']['allow_ssh'] = true
 default['roshicookbook']['http_allow_in_source'] = '172.31.32.0/24'
 default['roshicookbook']['http_allow_in_port'] = 80
+
+# web
+default['roshicookbook']['http_service']['user'] = 'www-data'
+default['roshicookbook']['http_service']['worker_processes'] = 4
+default['roshicookbook']['http_service']['worker_connections'] = 768
+
+# roshi
+default['roshicookbook']['roshi-server']['port'] = 6302
+
+# redis
+default['roshicookbook']['redis']['port'] = 6379
--- recipes/web.rb
+++ recipes/web.rb
@@ -9,6 +9,12 @@ package 'nginx' do
   action :install
 end
 
+# Overwrite nginx configuration with template resource.
+# This resource should enable reverse proxy functionality.
+template '/etc/nginx/nginx.conf' do
+  source 'nginx.conf.erb'
+end
+
 # Enable and start nginx service.
 service 'nginx' do
   action [ :enable, :start ]
